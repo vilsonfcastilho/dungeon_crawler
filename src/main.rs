04c807f2435e89caab88ddcea649fbc4afbe5998
Vilson Castilho <vilsonfcastilho@gmail.com>
2024-09-17 09:55:35 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+mod camera;
+mod map;
+mod map_builder;
+mod player;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
+
+    pub use crate::camera::*;
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
+
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+}
+
+use prelude::*;
+
+struct State {
+    map: Map,
+    player: Player,
+    camera: Camera,
+}
+
+impl State {
+    fn new() -> Self {
+        let mut rng: RandomNumberGenerator = RandomNumberGenerator::new();
+        let map_builder: MapBuilder = MapBuilder::new(&mut rng);
+
+        return Self {
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
+        };
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx, &self.camera);
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.player.render(ctx, &self.camera);
+    }
+}
+
+fn main() -> BError {
+    let context: BTerm = BTermBuilder::new()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .build()?;
+
+    main_loop(context, State::new())
+}
